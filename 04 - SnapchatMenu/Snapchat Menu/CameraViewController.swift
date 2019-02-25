37f7668a922ec8e80d3505b32fc5407f2521b485
Shuai Hao <shuaihao@ShuaideiMac.local>
2019-02-25 23:54:06 +0800

    day04 部分完成

--- 04 - SnapchatMenu/Snapchat Menu/CameraViewController.swift
+++ 04 - SnapchatMenu/Snapchat Menu/CameraViewController.swift
@@ -7,24 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
+    
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    var captureSession : AVCaptureSession?
+    var capturePhotoOutput :  AVCapturePhotoOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+            return
+        }
+        
+        var error : NSError?
+        var input : AVCaptureDeviceInput!
+        
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if error == nil && (captureSession?.canAddInput(input))!{
+            captureSession?.addInput(input)
+            
+            let settings = AVCapturePhotoSettings()
+            let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+            let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                                 kCVPixelBufferWidthKey as String: 160,
+                                 kCVPixelBufferHeightKey as String: 160,
+            ]
+            settings.previewPhotoFormat = previewFormat
+            
+            capturePhotoOutput = AVCapturePhotoOutput()
+            capturePhotoOutput?.capturePhoto(with: settings, delegate: self)
+        }
+        
         
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if captureSession?.canAddOutput(output) != nil {
+            return
+        }
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+        previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraView.layer.addSublayer(previewLayer!)
+        captureSession?.startRunning()
+        
     }
-    */
+    
+    func didPressTakePhoto() {
+        
+        print("拍照")
+    }
+    
+    var didTakePhoto = Bool()
+    
+    func didPressTakeAnother() {
+        if didTakePhoto == true {
+            tempImageView.isHidden = true
+            didTakePhoto = false
+        } else {
+            captureSession?.startRunning()
+            didTakePhoto = true
+            didPressTakePhoto()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        didPressTakeAnother()
+    }
 
 }
