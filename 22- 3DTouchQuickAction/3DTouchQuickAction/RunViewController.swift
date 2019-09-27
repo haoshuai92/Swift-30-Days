2ade75be984c36cffa59c9a2c1aea0b055cd26e0
Shuai Hao <shuaihao@ShuaideMacBook-Pro.local>
2019-09-27 14:03:09 +0800

    day 22 完毕

---
+++ 22- 3DTouchQuickAction/3DTouchQuickAction/RunViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RunViewController.swift
+//  3DTouchQuickAction
+//
+//  Created by Shuai Hao on 2019/9/26.
+//  Copyright © 2019 Shuai Hao. All rights reserved.
+//
+
+import UIKit
+
+class RunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 22- 3DTouchQuickAction/3DTouchQuickAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3DTouchQuickAction
+//
+//  Created by Shuai Hao on 2019/9/26.
+//  Copyright © 2019 Shuai Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
